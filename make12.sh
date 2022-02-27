7c66d55077f751f3c195841c66779fd2002d16a4
dp0148755 <dipeshtechmirror@gmail.com>
2022-02-27 10:57:58 +0530

    nothing

--- make12.sh
+++ make12.sh
@@ -9,6 +9,25 @@ if ! (ls $systempath/apex | grep -q ".apex$") ;then
    7z x -y $thispath/12/com.android.vndk.current.apex.7z -o$systempath/apex/ > /dev/null 2>&1
 fi
 
+# Clean up default apex state
+sed -i '/ro.apex.updatable/d' $systemdir/build.prop
+sed -i '/ro.apex.updatable/d' $systemdir/product/etc/build.prop
+sed -i '/ro.apex.updatable/d' $systemdir/system_ext/etc/build.prop
+
+apex_flatten() {
+	# Force use flatten apex
+  echo "" >> $systemdir/product/etc/build.prop
+  echo "# Apex state" >> $systemdir/product/etc/build.prop  
+  echo "ro.apex.updatable=false" >> $systemdir/product/etc/build.prop
+  
+  echo "$ENABLE_CLEAN_APEX_FILES"
+  local apex_files=$(ls $systemdir/apex | grep ".apex$")
+  for apex in $apex_files ;do
+    if [ -f $systemdir/apex/$apex ];then
+      rm -rf $systemdir/apex/$apex
+    fi
+  done
+
 # Make VNDK symlinks
 rm -rf $systempath/lib/vndk-30 $systempath/lib/vndk-sp-30
 rm -rf $systempath/lib/vndk-29 $systempath/lib/vndk-sp-29
@@ -36,6 +55,24 @@ ln -s  /apex/com.android.vndk.v28/lib64 $systempath/lib64/vndk-sp-28
 7z x -y $thispath/12/com.android.vndk.v29.apex.7z -o$systempath/apex/ 2>/dev/null >> $systempath/zip.log
 7z x -y $thispath/12/com.android.vndk.v30.apex.7z -o$systempath/apex/ 2>/dev/null >> $systempath/zip.log
 rm -rf $systempath/zip.log
+
+  # Add aosp apex
+  if ! apex_file ;then
+    7z x -y $thispath/11/apex_common.7z -o$systempath/apex/ > /dev/null 2>&1
+    android_art_debug_check() {
+      apex_ls | grep -q "art.debug" 
+    }
+    android_art_release_check() {
+      apex_ls | grep -q "art.release"
+    }
+    if android_art_debug_check ;then
+      7z x -y $thispath/11/art.debug.7z -o$systempath/apex/ > /dev/null 2>&1
+    fi
+
+    if android_art_release_check ;then
+      7z x -y $thispath/11/art.release.7z -o$systemdpath/apex/ > /dev/null 2>&1
+    fi
+  fi
 
 # Fix vintf for different vndk version
 manifest_file="$systempath/system_ext/etc/vintf/manifest.xml"
